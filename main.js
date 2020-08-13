fe31535194c69ad1e448b12fddef9cc3d52aa315
Kevin Hill <kahill2014@gmail.com>
2020-08-13 17:52:58 -0500

    Add funtionality to randomly generate a background color before transitioning to the next screen

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 $(function(){
-    var colors = ["#0081FE", "#C86E4C", "#2BD566", "#513E78"];
+    var colors = [];
+    colors["blue"] = "#0081FE";
+    colors["orange"] = "#C86E4C";
+    colors["green"] = "#2BD566";
+    colors["purple"] = "#513E78";
+    var colorValues = Object.values(colors);
+    var keyValues = Object.keys(colors);
 
     // Set background color programatically for each button
     $("#color-select a").each(function(index){
-        $(this).css("background-color", colors[index]);
-        $(this).css("box-shadow", "0 0 10px 5px " + colors[index])
+        $(this).css("background-color", colorValues[index]);
+        $(this).css("box-shadow", "0 0 10px 5px " + colorValues[index])
     });
 
     // Introduction screen animation
@@ -21,10 +27,37 @@ $(function(){
     // Color select screen handler
     $("#color-select a").click(function(e){
         e.preventDefault();
+        var tempThis = this;
+
+        $("#color-select").fadeOut(1000, function(){
+            var colorStringLength = keyValues[$("#color-select a").index(tempThis)].length;
+
+            var changeColor = setInterval(function(){
+                $("body").css("background-color", getRandomColor());
+                colorStringLength--;
+
+                if(colorStringLength == 0){
+                    $("body").css("background-color", colorValues[$("#color-select a").index(tempThis)]);
+                    clearInterval(changeColor)
+                }
+            }, 1000);
+        })
     });
+
+    // Increase blur when hovering over a color
     $("#color-select a").hover(function(e){
-        $(this).css("box-shadow", "0 0 20px 20px " + colors[$("#color-select a").index(this)]);
+        $(this).css("box-shadow", "0 0 20px 20px " + colorValues[$("#color-select a").index(this)]);
     }, function(){
-        $(this).css("box-shadow", "0 0 10px 5px " + colors[$("#color-select a").index(this)]);
+        $(this).css("box-shadow", "0 0 10px 5px " + colorValues[$("#color-select a").index(this)]);
     });
+
+    // Source: https://stackoverflow.com/questions/1484506/random-color-generator
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
 })
