d2e382a9dd31296ac2d2dbc32a33b25fefc6c2a1
Kevin Hill <kahill2014@gmail.com>
2020-08-13 16:55:30 -0500

    Add introduction screen animation

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function(){
+    setTimeout(function(){
+        $("#intro-screen").fadeIn(2000);
+    }, 1000);
+    setTimeout(function(){
+        $("#intro-screen").fadeOut(2000);
+    }, 5000);
+})
