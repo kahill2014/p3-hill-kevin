1a7b6299c99977c7bc0aa944e2327d8c3ce2f196
Kevin Hill <kahill2014@gmail.com>
2020-08-13 17:11:45 -0500

    Add fade in for color selection

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 $(function(){
-    setTimeout(function(){
-        $("#intro-screen").fadeIn(2000);
-    }, 1000);
-    setTimeout(function(){
-        $("#intro-screen").fadeOut(2000);
-    }, 5000);
+    var colors = ["#0081FE", "#C86E4C", "#2BD566", "#513E78"];
+
+    // Set background color programatically for each button
+    $("#color-select a").each(function(index){
+        $(this).css("background-color", colors[index]);
+    });
+
+    // Introduction screen animation
+    // setTimeout(function(){
+    //     $("#intro-screen").fadeIn(2000);
+    // }, 1000);
+    // setTimeout(function(){
+    //     $("#intro-screen").fadeOut(2000, function(){
+    //         $("#color-select").fadeIn(2000);
+    //     });
+    // }, 5000);
+    $("#color-select").fadeIn(2000);
+
+    // Color select screen handler
+    $("#color-select a").click(function(e){
+        e.preventDefault();
+    });
 })
